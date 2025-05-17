ef5a9fa1ffa99ae98c6d1116a0c50ff116fd0e77
enyaschaerrer <enya.schaerrer@gmail.com>
2025-05-17 17:19:02 +0200

    Update Cle.java
    class Cle added, with method 'unlockZone'

--- src/objet/Cle.java
+++ src/objet/Cle.java
@@ -1,5 +1,8 @@
 package objet;
 
 public class Cle {
-    
+    public void unlockZone(Zone zone){
+        zone.unlock();
+        System.out.println("La zone " + zone.getName() + " est déverouillée. ");
+    }
 }
