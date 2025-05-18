c74be14cd3a945e9cb2aab67d9e52c87f8cb81f4
enyaschaerrer <enya.schaerrer@gmail.com>
2025-05-18 17:19:09 +0200

    Update Cle.java
    rajout de l'extends et code du super

--- src/objet/Cle.java
+++ src/objet/Cle.java
@@ -1,8 +1,18 @@
 package objet;
 
-public class Cle {
+public class Cle extends Objet {
+
+    public Cle(Zone zone){
+        super(zone);
+    }
+
     public void unlockZone(Zone zone){
         zone.unlock();
         System.out.println("La zone " + zone.getName() + " est déverouillée. ");
+    }
+
+    @Override
+    public String inspect(Objet objet){
+        return "L'objet est une clé. ";
     }
 }
