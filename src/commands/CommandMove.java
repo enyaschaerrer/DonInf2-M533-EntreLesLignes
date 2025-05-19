f0cf23c301893d75b83fcdc76c7ce1c3f868ac9b
enyaschaerrer <enya.schaerrer@gmail.com>
2025-05-19 14:46:48 +0200

    changes in main

--- src/commands/CommandMove.java
+++ src/commands/CommandMove.java
@@ -1,13 +1,19 @@
+package commands;
+
 public class CommandMove extends Command {
     private String direction;
 
     public CommandMove(String direction) {
         super("move", "Be able to move foward, backwards, left or right.");
-        this.direction = direction;
+
+        if (!(direction.equals("left")||direction.equals("right")||direction.equals("top")||direction.equals("bottom"))) {
+            System.out.println("The direction isn't valid.");
+        }
+        else {
+            this.direction = direction;
     }
 
     @Override
     protected String doExecute(Player player, Zone zone) {
        
 }
-
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,30 @@
 package main;
 
-
 import examples.ConsoleStylingExample;
 import examples.StringManipulation;
 import examples.UserInputExample;
 import utils.Color;
 import utils.StringStyling;
 import utils.Style;
+import zone.Zone;
 
 public class Main {
 
     public static void main(String[] args) {
         // you can safely remove those examples lines
-        StringManipulation.Example();
-        ConsoleStylingExample.Example();
-        UserInputExample.Example();
+        /*
+         * StringManipulation.Example();
+         * ConsoleStylingExample.Example();
+         * UserInputExample.Example();
+         */
+        Game game = new Game();
+
+        Zone zone1 = new Zone("désert", "étendue de sable", "locked");
+        Zone zone2 = new Zone("forêt", "étendue de sapins", "locked");
+        Zone zone3 = new Zone("lac", "étendue d'eau profonde", "locked");
+        Zone zone4 = new Zone("étang", "étendue d'eau stagnante", "locked");
 
         System.out.println(StringStyling.StyleString("Starting...", Style.ITALIC, Color.BLACK));
-        Game game = new Game();
         game.run();
         System.out.println(StringStyling.StyleString("Terminating...", Style.ITALIC, Color.BLACK));
     }
